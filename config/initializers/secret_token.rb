4468079aec5caf9a6fbb83f495e2b6f2790a7a01
Morodeer <morodeer@gmail.com>
2013-07-05 17:40:50 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailstutLaptop::Application.config.secret_key_base = 'c58c52e9814a1b5774ab19fbd61ef79aba735014bc9d563fdaade6cffa045e5c65b6d3f0875d37221904d64e6746aca5b9d7af95fd1afe647ac28eaf15e4c87e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailstutLaptop::Application.config.session_store :cookie_store, key: '_railstut_laptop_session'
